275ea024c851b4db3340dce8818ec748708ee0dc
sudwiptokm <sudwiptokmondal@gmail.com>
2024-08-30 00:46:38 +0600

    feat: markdown rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,22 @@
-import Link from 'next/link';
-import { getSortedPostsData, PostData } from './lib/posts'; // Ensure the correct path
+import Link from "next/link";
+import { type PostData, getSortedPostsData } from "./lib/posts"; // Ensure the correct path
 
 export default function Home() {
-  const allPostsData: PostData[] = getSortedPostsData();
-  console.log(allPostsData);
+	const allPostsData: PostData[] = getSortedPostsData();
+	console.log(allPostsData);
 
-  return (
-    <section className="w-full h-screen flex flex-col justify-start p-5">
-      <h1 className='text-4xl font-bold py-5'>Ontario Pulse</h1>
-      <ul className='border w-full'>
-        {allPostsData.map(({ id, date, title }) => (
-          <li className='border' key={id}>
-            <Link href={`/posts/${id}`}>
-              {title}
-            </Link>
-            <br />
-            <small>{date}</small>
-          </li>
-        ))}
-      </ul>
-    </section>
-  );
+	return (
+		<section className="w-full h-screen flex flex-col justify-start p-5">
+			<h1 className="text-4xl font-bold py-5">Ontario Pulse</h1>
+			<ul className="border w-full">
+				{allPostsData.map(({ id, date, title }) => (
+					<li className="border" key={id}>
+						<Link href={`/posts/${id}`}>{title}</Link>
+						<br />
+						<small>{date}</small>
+					</li>
+				))}
+			</ul>
+		</section>
+	);
 }
--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,35 +1,44 @@
-import { PostData, getPostData, getAllPostIds } from '../../lib/posts';
-import { Metadata } from 'next';
+import type { Metadata } from "next";
+import Blog from "../../components/Blog";
+import { type PostData, getAllPostIds, getPostData } from "../../lib/posts";
 
-export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
-  const postData = await getPostData(params.id);
-  return {
-    title: postData.title,
-    description: postData.description,
-    openGraph: {
-      images: [
-        {
-          url: postData.ogImage,
-          alt: postData.title,
-        },
-      ],
-      title: postData.title,
-      description: postData.description,
-    },
-  };
+export async function generateMetadata({
+	params,
+}: { params: { id: string } }): Promise<Metadata> {
+	const postData = await getPostData(params.id);
+	return {
+		title: postData.title,
+		description: postData.description,
+		openGraph: {
+			images: [
+				{
+					url: postData.ogImage,
+					alt: postData.title,
+				},
+			],
+			title: postData.title,
+			description: postData.description,
+		},
+	};
 }
 
 export default async function Post({ params }: { params: { id: string } }) {
-  const postData: PostData = await getPostData(params.id);
-  return (
-    <article>
-      <h1 className='text-2xl font-bold leading-loose'>{postData.title}</h1>
-      <div className='space-y-5' dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-    </article>
-  );
+	const postData: PostData = await getPostData(params.id);
+	console.log({ params });
+	return (
+		// <article>
+		// 	<h1 className="text-2xl font-bold leading-loose">{postData.title}</h1>
+		// 	<div
+		// 		className="space-y-5"
+		// 		dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+		// 	/>
+		// </article>
+
+		<Blog postName={postData.id} />
+	);
 }
 
 export async function generateStaticParams() {
-  const ids = getAllPostIds();
-  return ids.map(id => ({ id: id.id }));
+	const ids = getAllPostIds();
+	return ids.map((id) => ({ id: id.id }));
 }
